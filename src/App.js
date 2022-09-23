b6373e56919e93c077303b73d4ffa1dffae2e50e
Bijaylaxmi Behera <72284560+bijaylaxmibehera@users.noreply.github.com>
2022-09-23 23:17:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./styles.css";
+//database
+const emojiDictionary = {
+  "😀": "Happy Face",
+  "❤️": "Love",
+  "🎈": "Balloon",
+  "🥰": "In Love Face",
+  "😉": "Wink Face",
+  "😂": " Face with Tears of Joy",
+  "😋": "Face Savouring Delicious Food",
+  "😎": "Cool"
+};
+//convert object to array
+const emojisWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  var [meaning, setMeaning] = useState("");
+  function emojiClickHandler(emoji) {
+    //processing
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning); //set output
+  }
+  function inputChangeHandler(event) {
+    //processing
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput]; //input
+    if (meaning === undefined) {
+      meaning = "we don't have this in our database";
+    }
+    setMeaning(meaning); //set output
+  }
+
+  return (
+    <div className="App">
+      <h1>inside outtt</h1>
+      <input onChange={inputChangeHandler} />
+      {/* output */}
+      <h2>
+        Meaning <em style={{ color: "#651fff" }}>{meaning}</em>
+      </h2>
+      <h3>emojis we know</h3>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span onClick={() => emojiClickHandler(emoji)} id="emoji">
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
